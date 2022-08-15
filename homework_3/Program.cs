465e8429560d60f7114e75259bc6ed6933cef449
IrinaLitv <Litvinenko-Irina@yandex.ru>
2022-08-15 16:45:22 +0300

    add homework 3

---
+++ homework_3/Program.cs
@@ -0,0 +1,59 @@
+﻿// Домашняя работа по семинару 3.
+
+// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// Console.WriteLine("Введите пятизначное число: ");
+// int number =Convert.ToInt32(Console.ReadLine());
+// int num1 = number / 10000;
+// int num5 = number % 10;
+// int num2 = number % 10000 / 1000;
+// int num4 = number % 100 / 10;
+// if(num1 == num5 && num2 == num4)
+// {
+//     Console.WriteLine($"Число: {number} - палиндром ");
+// }
+// else Console.WriteLine($"Число: {number} - НЕ палиндром ");
+// if (number < 10000 || number > 99999)
+// {
+//     Console.WriteLine("Ошибка ввода!");
+// }
+
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// Console.WriteLine("Введите х1: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите y1: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите z1: ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите х2: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите y2: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите z2: ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+
+// double result = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2) + Math.Pow((z2 - z1),2));
+
+// Console.WriteLine($"Расстояние между точками в 3D пространстве = {Math.Round(result,2)}");
+
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int number =Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine($"Кубом от числа {i} = {Math.Pow(i,3)}");
+}
